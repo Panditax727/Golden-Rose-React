083b9383383699fa30ba46281ea6acfdd30ce39b
Pizarro777 <127531692+Panditax727@users.noreply.github.com>
2025-10-16 15:23:48 -0300

    Create Administrador.jsx

---
+++ src/pages/admin/Administrador.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Administrador() {
+  return (
+    <>
+    <aside>
+        <header class="mb-4 p-3">
+            <h4 class="d-flex align-items-center mb-0">
+                <img src="public/icon.png" alt="Golden Rose" width="48" height="48" style="border-radius: 12px; box-shadow: 0 2px 8px #ffb6c1; background: #fff; padding: 2px;"/>
+                <span style="font-size: 1.7rem; font-weight: bold; letter-spacing: 1px; color: #FFD700; text-shadow: 1px 1px 8px #ffb6c1;">Golden Rose</span>
+            </h4>
+        </header>        
+    </aside>
+    </>
+  )
+}
+
+export default Administrador
