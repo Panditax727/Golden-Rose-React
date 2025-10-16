ef5df654fbaff56d32f160898f9f991cc549e166
Pizarro777 <127531692+Panditax727@users.noreply.github.com>
2025-10-15 21:27:22 -0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer class="text-center py-5 border-top mt-5 bg-dark text-light">
+      <p class="mb-0">&copy; 2025 Golden Rose. Todos los derechos reservados.</p>
+    </footer>
+  )
+}
+
+export default Footer
