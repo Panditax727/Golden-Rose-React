4dc3574bfb79247f024b1302958019f6933ff202
Pizarro777 <127531692+Panditax727@users.noreply.github.com>
2025-10-18 23:03:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <Navbar /> // dtrdtdrtdtdt
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
