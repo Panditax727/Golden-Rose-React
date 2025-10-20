1d8796c8204c3c15fc578872a28cad8271f7a8dc
Pizarro777 <127531692+Panditax727@users.noreply.github.com>
2025-10-20 20:55:16 -0300

    Update App.jsx
    add: HomeClient

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,40 @@ import SkinDetailPage from "./pages/SkinDetailPage";
 import Carrito from "./pages/Carrito";
 import Contacto from "./pages/Contacto";
 import Administrador from "./pages/admin/Administrador";
+import HomeClient from "./pages/HomeClient";
 
 function App() {
 
   return (
     <>
       <Routes>
+        {/* Rutas Públicas */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Administrador/>} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/skin/:id" element={<SkinDetailPage />} />
+
+        <Route 
+          path="/api/home" 
+          element={
+          <HomeClient />
+        } 
+        />
+
+        {/* Proteger Rutas */}
+        <Route 
+          path="/admin" 
+          element={
+            
+          <Administrador/>
+        } 
+        />
+
+        {/* Ruta inexistente (404) */}
+        <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
       </Routes>
     </>
   )
